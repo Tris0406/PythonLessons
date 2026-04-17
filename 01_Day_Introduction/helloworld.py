3d656afd9b0e5761e39a3ebce3513d5a0e374111
Nikita Gordhan <96737737+Tris0406@users.noreply.github.com>
2026-04-17 10:52:53 +0200

    Update helloworld.py
    added notes on data types

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -16,10 +16,20 @@ print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name': 'Asabeneh'}))  # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Tuple
+
+print(type(10))                    
+# Int - •	whole numbers, •	no decimal point, •	used for counting, indexing and calculations 
+print(type(3.14))                  
+# Float - •	numbers with decimal points , •	used for measurements, money and averages
+print(type(1 + 3j))                
+# Complex - •	numbers with real and imaginary parts, •	written as a + bj, •	j represents √-1, •	used in advanced math, physics and engineering 
+print(type('Asabeneh'))            
+# String - •	text data, •	written inside single ‘’ or double “” quotes 
+print(type([1, 2, 3]))            
+# List - •	stores multiple values, •	ordered and changeable (mutable), •	written using [], •	used to store items like number, names, etc 
+print(type({'name': 'Asabeneh'})) 
+# Dictionary - •	stores data as a key : value pairs, •	written using {}, •	used to store structured data  
+print(type({9.8, 3.14, 2.7}))     
+# Set - •	stores unique values only, •	unordered, •	written in {} without key : value pairs, •	used when you don’t want duplicate values 
+print(type((9.8, 3.14, 2.7)))      
+# Tulpe - •	look similar to lists but use (), •	ordered and unchangeable
